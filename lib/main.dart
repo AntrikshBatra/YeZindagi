8bf43f819113ed090e7b85bffa6a04f8599efbdf
Antriksh Batra <67945400+AntrikshBatra@users.noreply.github.com>
2023-01-09 02:18:51 +0530

    finally

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedgetstartedwidget/GeneratedGetstartedWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedsignupwidget/GeneratedSignupWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedbraingymwidget/GeneratedBraingymWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedtherapistwidget2/GeneratedTherapistWidget2.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedtherapistinfo1widget/GeneratedTherapistinfo1Widget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedtherapistinfo2widget/GeneratedTherapistinfo2Widget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedtherapistinfo3widget/GeneratedTherapistinfo3Widget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedmusicwidget6/GeneratedMusicWidget6.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedventingwidget/GeneratedVentingWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedalbumwidget/GeneratedAlbumWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedplayerwidget/GeneratedPlayerWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatednewventwidget/GeneratedNewventWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatednewvent2widget/GeneratedNewvent2Widget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatednewvent3widget/GeneratedNewvent3Widget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatednewvent4widget/GeneratedNewvent4Widget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedaccordianwidget1/GeneratedACCORDIANWidget1.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedsidebarnavigationwidget/GeneratedSidebarnavigationWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatededitprofilewidget/GeneratedEditprofileWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedwireframewidget/GeneratedWireframeWidget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedwireframe2widget/GeneratedWireframe2Widget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedwireframe3widget/GeneratedWireframe3Widget.dart';
+import 'package:flutterapp/ye_20zindagiapp/generatedwireframe4widget/GeneratedWireframe4Widget.dart';
+
+class Ye_20ZindagiApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedGetstartedWidget',
+      routes: {
+        '/GeneratedGetstartedWidget': (context) => GeneratedGetstartedWidget(),
+        '/GeneratedSignupWidget': (context) => GeneratedSignupWidget(),
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedBraingymWidget': (context) => GeneratedBraingymWidget(),
+        '/GeneratedTherapistWidget2': (context) => GeneratedTherapistWidget2(),
+        '/GeneratedTherapistinfo1Widget': (context) =>
+            GeneratedTherapistinfo1Widget(),
+        '/GeneratedTherapistinfo2Widget': (context) =>
+            GeneratedTherapistinfo2Widget(),
+        '/GeneratedTherapistinfo3Widget': (context) =>
+            GeneratedTherapistinfo3Widget(),
+        '/GeneratedMusicWidget6': (context) => GeneratedMusicWidget6(),
+        '/GeneratedVentingWidget': (context) => GeneratedVentingWidget(),
+        '/GeneratedAlbumWidget': (context) => GeneratedAlbumWidget(),
+        '/GeneratedPlayerWidget': (context) => GeneratedPlayerWidget(),
+        '/GeneratedNewventWidget': (context) => GeneratedNewventWidget(),
+        '/GeneratedNewvent2Widget': (context) => GeneratedNewvent2Widget(),
+        '/GeneratedNewvent3Widget': (context) => GeneratedNewvent3Widget(),
+        '/GeneratedNewvent4Widget': (context) => GeneratedNewvent4Widget(),
+        '/GeneratedACCORDIANWidget1': (context) => GeneratedACCORDIANWidget1(),
+        '/GeneratedSidebarnavigationWidget': (context) =>
+            GeneratedSidebarnavigationWidget(),
+        '/GeneratedEditprofileWidget': (context) =>
+            GeneratedEditprofileWidget(),
+        '/GeneratedWireframeWidget': (context) => GeneratedWireframeWidget(),
+        '/GeneratedWireframe2Widget': (context) => GeneratedWireframe2Widget(),
+        '/GeneratedWireframe3Widget': (context) => GeneratedWireframe3Widget(),
+        '/GeneratedWireframe4Widget': (context) => GeneratedWireframe4Widget(),
+      },
+    );
+  }
+}
+
+void main() {
+  runApp(Ye_20ZindagiApp());
+}
